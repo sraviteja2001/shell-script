27b3d65c6d145a1ed3a64a33aa00c5f394f795fc
raviteja <suragalaraviteja@gmail.com>
2026-03-13 14:44:14 +0530

    03-shell

---
+++ 03-variablesh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+PERSON1=JOHN
+PERSON2=RAVI
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell scripting $PERSON1"
